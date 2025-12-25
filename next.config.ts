aef4263bf8fee6b0d4c0b0b6d543093b7ea95cff
DearShervin <sherviniranaghideh@gmail.com>
2025-12-26 03:20:16 +0330

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/portfolio',
+  basePath: '/studio',
+  assetPrefix: '/studio',
   typescript: {
     ignoreBuildErrors: true,
   },
